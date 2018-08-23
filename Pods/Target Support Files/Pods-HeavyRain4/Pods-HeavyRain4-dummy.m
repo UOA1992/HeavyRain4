7b06ead866de267f9f411fd71aa222f2f40be5f1
UOA1992 <UOA1992@github.com>
2018-08-23 16:57:26 -0400

    08 23 18

---
+++ Pods/Target Support Files/Pods-HeavyRain4/Pods-HeavyRain4-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_HeavyRain4 : NSObject
+@end
+@implementation PodsDummy_Pods_HeavyRain4
+@end
---
+++ build/Pods.build/Debug-iphoneos/Pods-HeavyRain4.build/DerivedSources/Pods_HeavyRain4_vers.c
@@ -0,0 +1,5 @@
+ extern const unsigned char Pods_HeavyRain4VersionString[];
+ extern const double Pods_HeavyRain4VersionNumber;
+
+ const unsigned char Pods_HeavyRain4VersionString[] __attribute__ ((used)) = "@(#)PROGRAM:Pods_HeavyRain4  PROJECT:Pods-1" "\n";
+ const double Pods_HeavyRain4VersionNumber __attribute__ ((used)) = (double)1.;
